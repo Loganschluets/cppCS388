876c857ae39104b4b2499835fd91e7fd012dd2c0
nithinreddy2642 <nithinreddy2303@gmail.com>
2025-04-16 13:37:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,94 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "constants.hpp"
 
-using namespace std; 
+using namespace std;
 
-void parseCommand(string command);
+void parseCommand(const string& command);
 void getProcesses();
-
-
+void showCPU();
+void showRAM();
+void showNetwork();
+void showConnections();
+void killProcess(const string& pid);
+void showHelp();
 
 int main() {
     cout << Constants::WELCOME_MESSAGE;
-    string command = "";
+    string command;
 
-    // terminal program driven by while loop and leaves on exit
-    while (command != Constants::EXIT_COMMAND) {
+    while (true) {
         cout << Constants::TERMINAL_PROMPT;
-        cout << "Type a number: ";
-        cin >> command;
+        getline(cin, command);
+        if (command == Constants::EXIT_COMMAND) break;
         parseCommand(command);
     }
+
     return 0;
 }
 
-//this function will be used to parse the different commands we add
-//these conditionals can lead to other functions depending on how much logic the command includes
-//any example of this is the conditional for processes, if met this executes the function to show all processes
-void parseCommand(string command) {
+void parseCommand(const string& command) {
     if (command == "processes") {
         getProcesses();
+    } else if (command == "cpu") {
+        showCPU();
+    } else if (command == "ram") {
+        showRAM();
+    } else if (command == "network") {
+        showNetwork();
+    } else if (command == "connections") {
+        showConnections();
+    } else if (command.rfind("kill", 0) == 0) {
+        istringstream iss(command);
+        string cmd, pid;
+        iss >> cmd >> pid;
+        if (!pid.empty()) killProcess(pid);
+        else cout << "Please provide a PID to kill.\n";
+    } else if (command == "clear") {
+        system("clear");
+    } else if (command == Constants::HELP_COMMAND) {
+        showHelp();
+    } else {
+        cout << "Invalid command. Type 'help' for a list of commands.\n";
     }
 }
 
 void getProcesses() {
-    FILE* pipe = popen("ps -e", "r");
-    if (!pipe) {
-        std::cerr << "Error opening pipe" << std::endl;
-        return;
-    }
-
-    char buffer[256];
-    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
-        std::cout << buffer;
-    }
-
-    pclose(pipe);
+    system("ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head");
 }
 
+void showCPU() {
+    system("top -bn1 | grep \"%Cpu\"");
+}
+
+void showRAM() {
+    system("free -h");
+}
+
+void showNetwork() {
+    system("ifconfig");
+}
+
+void showConnections() {
+    system("netstat -tunapl");
+}
+
+void killProcess(const string& pid) {
+    string cmd = "kill -9 " + pid;
+    int result = system(cmd.c_str());
+    if (result != 0) cout << "Failed to kill process " << pid << "\n";
+}
+
+void showHelp() {
+    cout << "\nAvailable Commands:\n";
+    cout << "  help          - Show this help message\n";
+    cout << "  processes     - List all running processes\n";
+    cout << "  cpu           - Show current CPU usage\n";
+    cout << "  ram           - Show current RAM usage\n";
+    cout << "  network       - Show network interface info\n";
+    cout << "  connections   - Show active TCP/UDP connections\n";
+    cout << "  kill <pid>    - Kill a process by its PID\n";
+    cout << "  clear         - Clear the terminal screen\n";
+    cout << "  exit          - Exit the program\n\n";
+}
